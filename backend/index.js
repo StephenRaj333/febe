6663e5e96788b48b73109a4307ebd5b63c92aa6e
stephen_M2P <stephen+M2P@m2pfintech.com>
2024-05-23 12:47:39 +0530

    testing atom code push

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 const app = express(); 
 const Port = 3000; 
 
-let info = [];  
-const data =  fs.readFileSync('./data.json','utf-8');      
-info.push(JSON.parse(data)); 
 
 app.get('/',(req,res) => { 
     res.send(info)
